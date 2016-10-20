61cdddc870f13a0acc53908e25ffdda151a5ab18
drupchen <hhdrupchen@gmail.com>
2016-10-20 12:57:01 +0200

    missing file

--- ༡༽ མགོན་པོ་ཀླུ་སྒྲུབ།/docx_txt.sh
+++ ༡༽ མགོན་པོ་ཀླུ་སྒྲུབ།/docx_txt.sh
@@ -3,28 +3,28 @@
 # sudo apt-get install docx2txt xlsx2csv
 # xlsx2csv requires to escape the spaces in the file names. The easiest is to remove all spaces from file names.
 
-FILES=./ཡིག་ཆ་འདིར་ཡོད།/*.docx
-for f in $FILES
-do
-  # extension="${f##*.}"
-  filename="${f#./ཡིག་ཆ་འདིར་ཡོད།/}"
-  filename="${filename%.*}"
-  echo "Converting $f"
-  #`docx2txt < $f > $filename.txt`
-  `pandoc -f docx -t plain --wrap=preserve  $f | sed  '/^$/d' > ./csvtxt/$filename.txt`
-  # uncomment this line to delete the source file.
-  # rm $f
-done
-
-#GILES=./ཡིག་ཆ་འདིར་ཡོད།/*.xlsx
-#for g in $GILES
+#FILES=./ཡིག་ཆ་འདིར་ཡོད།/*.docx
+#for f in $FILES
 #do
-#  # extension="${g##*.}"
-#  gilename="${g#./ཡིག་ཆ་འདིར་ཡོད།/}"
-#  gilename="${gilename%.*}"
-#  echo "Converting $g"
-#  `xlsx2csv $g | tr ';' ',' > ../../canon_notes/1-a-reinsert_notes/input/$gilename.csv`
-#  #`xlsx2csv $g | tr ';' ',' > ./csvtxt/$gilename.csv`
+#  # extension="${f##*.}"
+#  filename="${f#./ཡིག་ཆ་འདིར་ཡོད།/}"
+#  filename="${filename%.*}"
+#  echo "Converting $f"
+#  #`docx2txt < $f > $filename.txt`
+#  `pandoc -f docx -t plain --wrap=preserve  $f | sed  '/^$/d' > ./csvtxt/$filename.txt`
 #  # uncomment this line to delete the source file.
-#  # rm $g
+#  # rm $f
 #done
+
+GILES=./ཡིག་ཆ་འདིར་ཡོད།/*.xlsx
+for g in $GILES
+do
+  # extension="${g##*.}"
+  gilename="${g#./ཡིག་ཆ་འདིར་ཡོད།/}"
+  gilename="${gilename%.*}"
+  echo "Converting $g"
+  `xlsx2csv $g | tr ';' ',' > ../../canon_notes/1-a-reinsert_notes/input/$gilename.csv`
+  #`xlsx2csv $g | tr ';' ',' > ./csvtxt/$gilename.csv`
+  # uncomment this line to delete the source file.
+  # rm $g
+done
